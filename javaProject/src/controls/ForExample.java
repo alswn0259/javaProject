803be1bf3a106aa2ced9fbdf3b8e55363a3a4078
user <user@DESKTOP-695FP35>
2021-02-23 23:48:21 +0900

    Test

--- javaProject/src/controls/ForExample.java
+++ javaProject/src/controls/ForExample.java
@@ -3,7 +3,8 @@ package controls;
 public class ForExample {
 
 	public static void main(String[] args) {
-	// 1~10까지 합계
+	// 1~10의 합
+		System.out.println("안녕");
 		int sum = 0;
 		for (int i=1; i<=10; i++) {
 			if (i%2 == 0) {
@@ -11,6 +12,6 @@ public class ForExample {
 			System.out.println("i : " + i + "\t" + "sum : " + sum);
 			} 
 		}
-		System.out.println("1~10 짝수 합 : " + sum);
+		System.out.println("1~10 吏앹닔 �빀 : " + sum);
 		}
 	}
